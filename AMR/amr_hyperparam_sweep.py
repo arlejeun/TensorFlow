fad5038c6e04071f36988ff3bb6d4b5f4c714fc2
jamestwhedbee <jamestwhedbee@gmail.com>
2015-12-15 23:45:33 -0600

    grid search

---
+++ AMR/amr_hyperparam_sweep.py
@@ -0,0 +1,36 @@
+import itertools
+from multiprocessing import Pool
+import json
+import amr_net
+
+############
+#PARAMETERS#
+############
+
+NUM_PROCESS = 4
+
+hidden_units_grid = [16, 64, 256, 1024, 4096]
+input_keep_grid = [.01, .1, .25, .5, .75] 
+hidden_keep_grid = [.01, .1, .25, .5, .75]
+lr_grid = [.0001, .001, .01, .1, 1]
+rho_grid = [.1, .3, .5, .7, .9]
+feature_grid = [100, 1000, 10000, 100000, 524684]
+
+hyperparameter_grid = [hidden_units_grid, input_keep_grid, hidden_keep_grid, lr_grid, rho_grid, feature_grid]
+hyperparameter_list = list(itertools.product(*hyperparameter_grid))
+
+def main_star(hyperparams):	#pool.map does not support * operator
+	return amr_net.main(*hyperparams)
+
+try:
+    pool = Pool(NUM_PROCESS)
+    output = pool.map(main_star, hyperparameter_list)
+finally: # To make sure processes are closed in the end, even if errors happen
+    pool.close()
+    pool.join()
+
+output_file = 'amr.json'
+#Sort on lowest final test error
+sorted_output = sorted(output, key=lambda k: k['test_error'][-1]) 
+with open(output_file,'w') as f:
+	json.dump(sorted_output,f)
--- AMR/amr_net.py
+++ AMR/amr_net.py
@@ -6,149 +6,147 @@ import random
 from math import sqrt
 import tensorflow as tf
 
-#######
-#SETUP#
-#######
-
-tab_data = 'carbapenem.k10'
-
-#Too many columns for this machine, randomly select a subset
-col_limit = 10000
-col_total = 524684
-skip_cols = [0]+random.sample(range(1,col_total), col_total-col_limit) #Always skip row 0
-df = pd.read_csv(tab_data, sep='\t', skiprows=skip_cols, header=None)
-
-# Transpose, relabel, and scale
-amr_data_x = df.transpose()[1:]
-feature_cols = len(amr_data_x.columns)
-amr_data_x = preprocessing.scale(amr_data_x)
-
-#Get targets and encode as integers
-response_row = pd.read_csv(tab_data, sep='\t', nrows=1, header=None)
-response_col = np.array(response_row.transpose()[1:])
-
-#Format to work with multi-class classifier
-def split_y(y):
-	if y == 1: return [0,1]
-	else: return [1,0] 
-
-amr_data_y = np.array(map(split_y,response_col))
-
 ############
 #PARAMETERS#
 ############
 
 training_pct = 75
-hidden_units = 250
-input_keep = .1
-hidden_keep = .3
-lr = .001
-rho = .9
-steps = 101
+steps = 201
 
-##############
-#ARCHITECTURE#
-##############
+def main(hidden_units, input_keep, hidden_keep, lr, rho, features):
 
-def init_weights(shape):
-    return tf.Variable(tf.random_normal(shape) * 1./sqrt(shape[0]))
+    #######
+    #SETUP#
+    #######
 
-def model(X, w_h, w_o, p_drop_input, p_drop_hidden):
-    X = tf.nn.dropout(X, p_drop_input)
-    h = tf.nn.relu(tf.matmul(X, w_h))
-    h = tf.nn.dropout(h, p_drop_hidden)
+    tab_data = 'carbapenem.k10'
 
-    return tf.matmul(h, w_o)
+    #Too many columns for this machine, randomly select a subset
+    col_limit = features
+    col_total = 524684
+    skip_cols = [0]+random.sample(range(1,col_total), col_total-col_limit) #Always skip row 0
+    df = pd.read_csv(tab_data, sep='\t', skiprows=skip_cols, header=None)
 
-sess = tf.InteractiveSession()
+    # Transpose, relabel, and scale
+    amr_data_x = df.transpose()[1:]
+    feature_cols = len(amr_data_x.columns)
+    amr_data_x = preprocessing.scale(amr_data_x)
 
-X = tf.placeholder("float", [None, feature_cols])
-Y = tf.placeholder("float", [None, 2])
+    #Get targets and encode as integers
+    response_row = pd.read_csv(tab_data, sep='\t', nrows=1, header=None)
+    response_col = np.array(response_row.transpose()[1:])
 
-p_keep_input = tf.placeholder("float")
-p_keep_hidden = tf.placeholder("float")
+    #Format to work with multi-class classifier
+    def split_y(y):
+        if y == 1: return [0,1]
+        else: return [1,0] 
 
-w_h =  init_weights([feature_cols, hidden_units])
-w_o =  init_weights([hidden_units, 2])
+    amr_data_y = np.array(map(split_y,response_col))
 
-py_x = model(X, w_h, w_o, p_keep_input, p_keep_hidden)
+    ##############
+    #ARCHITECTURE#
+    ##############
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
-train_op = tf.train.RMSPropOptimizer(lr, rho).minimize(cost)
-predict_op = tf.argmax(py_x, 1)
+    def init_weights(shape):
+        return tf.Variable(tf.random_normal(shape) * 1./sqrt(shape[0]))
 
-#############
-#TENSORBOARD#
-#############
+    def model(X, w_h, w_o, p_drop_input, p_drop_hidden):
+        X = tf.nn.dropout(X, p_drop_input)
+        h = tf.nn.relu(tf.matmul(X, w_h))
+        h = tf.nn.dropout(h, p_drop_hidden)
 
-#Error
-with tf.name_scope("error") as scope:
+        return tf.matmul(h, w_o)
+
+    X = tf.placeholder("float", [None, feature_cols])
+    Y = tf.placeholder("float", [None, 2])
+
+    p_keep_input = tf.placeholder("float")
+    p_keep_hidden = tf.placeholder("float")
+
+    w_h =  init_weights([feature_cols, hidden_units])
+    w_o =  init_weights([hidden_units, 2])
+
+    py_x = model(X, w_h, w_o, p_keep_input, p_keep_hidden)
+
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
+    train_op = tf.train.RMSPropOptimizer(lr, rho).minimize(cost)
+    predict_op = tf.argmax(py_x, 1)
+
+    #############
+    #TENSORBOARD#
+    #############
+
+    #Base metrics
+    tp = tf.logical_and(tf.equal(tf.argmax(py_x,1),1),tf.equal(tf.argmax(Y,1),1))
+    fp = tf.logical_and(tf.equal(tf.argmax(py_x,1),1),tf.equal(tf.argmax(Y,1),0))
+    fn = tf.logical_and(tf.equal(tf.argmax(py_x,1),0),tf.equal(tf.argmax(Y,1),1))
+
+    #Error
     correct_prediction = tf.equal(tf.argmax(py_x,1), tf.argmax(Y,1))
     error = 1-tf.reduce_mean(tf.cast(correct_prediction, "float"))
     error_summary = tf.scalar_summary("error", error)
 
-#Precision
-with tf.name_scope("precision") as scope:
-    tp = tf.logical_and(tf.equal(tf.argmax(py_x,1),1),tf.equal(tf.argmax(Y,1),1))
-    fp = tf.logical_and(tf.equal(tf.argmax(py_x,1),1),tf.equal(tf.argmax(Y,1),0))
+    #Precision
     precision = tf.div(tf.reduce_sum(tf.cast(tp, "float")) ,tf.add(tf.reduce_sum(tf.cast(tp, "float")), tf.reduce_sum(tf.cast(fp, "float"))))
     precision_summary = tf.scalar_summary("precision", precision)
 
-#Recall
-with tf.name_scope("recall") as scope:
-    tp = tf.logical_and(tf.equal(tf.argmax(py_x,1),1),tf.equal(tf.argmax(Y,1),1))
-    fn = tf.logical_and(tf.equal(tf.argmax(py_x,1),0),tf.equal(tf.argmax(Y,1),1))
+    #Recall
     recall = tf.div(tf.reduce_sum(tf.cast(tp, "float")), tf.add(tf.reduce_sum(tf.cast(tp, "float")), tf.reduce_sum(tf.cast(fn, "float"))))
     recall_summary = tf.scalar_summary("recall", recall)
 
-#F1 score
-with tf.name_scope("f1_score") as scope:
+    #F1 score
     f1_score = 2 * tf.div(tf.mul(precision,recall),tf.add(precision,recall))
     f1_summary = tf.scalar_summary("f1_score", f1_score)
 
+    init = tf.initialize_all_variables()
+    sess = tf.Session()
+    sess.run(init)
 
-#Merge all the summaries and write them out to /tmp/amr_logs
-merged = tf.merge_all_summaries()
-writer = tf.train.SummaryWriter("/tmp/amr_logs", sess.graph_def)
-tf.initialize_all_variables().run()
+    ##########
+    #TRAINING#
+    ##########
 
-##########
-#TRAINING#
-##########
+    trX, teX, trY, teY = train_test_split(amr_data_x, amr_data_y, train_size=training_pct/100.0)
+    test_error = []
+    train_error = []
+    prec = []
+    rec = []
+    f1 = []
 
-trX, teX, trY, teY = train_test_split(amr_data_x, amr_data_y, train_size=training_pct/100.0)
+    for i in range(steps):
 
-for i in range(steps):
+        if i % 10 == 0:  # Record test/train error and other scores
+            test_feed = {X: teX, Y: teY, p_keep_input: 1.0, p_keep_hidden:1.0}
+            train_feed = {X: trX, Y: trY, p_keep_input: 1.0, p_keep_hidden:1.0}
 
-    if i % 10 == 0:  # Record summary data, and the test/train error
-        test_feed = {X: teX, Y: teY, p_keep_input: 1.0, p_keep_hidden:1.0}
-        test_result = sess.run([merged, error], feed_dict=test_feed)
-        test_summary_str = test_result[0]
-        test_err = test_result[1]
-        writer.add_summary(test_summary_str, i)
+            test_result = sess.run(error, feed_dict=test_feed)
+            train_result = sess.run(error, feed_dict=train_feed)
+            precision_result = sess.run(precision, feed_dict=test_feed)     
+            recall_result = sess.run(recall, feed_dict=test_feed)       
+            f1_result = sess.run(f1_score, feed_dict=test_feed)  
 
-        train_feed = {X: trX, Y: trY, p_keep_input: 1.0, p_keep_hidden:1.0}
-        train_result = sess.run([merged, error], feed_dict=train_feed)
-        train_summary_str = train_result[0]
-        train_err = train_result[1]
-        writer.add_summary(train_summary_str, i)
-
-        precision_result = sess.run([merged, precision], feed_dict=test_feed)  
-        precision_summary_str = precision_result[0]
-        prec = precision_result[1]
-        writer.add_summary(precision_summary_str, i)      
-
-        recall_result = sess.run([merged, recall], feed_dict=test_feed)  
-        recall_summary_str = recall_result[0]
-        rec = recall_result[1]
-        writer.add_summary(recall_summary_str, i)      
-
-        f1_result = sess.run([merged, f1_score], feed_dict=test_feed)  
-        f1_summary_str = f1_result[0]
-        f1 = f1_result[1]
-        writer.add_summary(f1_summary_str, i)      
+            #These need to be floats to dump as json later
+            test_error.append(float(test_result))
+            train_error.append(float(train_result))   
+            prec.append(float(precision_result))
+            rec.append(float(recall_result))
+            f1.append(float(f1_result))
         
-        print("Train Error, Test Error, Precision, Recall, F1 at step %s: %s, %s, %s, %s, %s" % (i, train_err, test_err, prec, rec, f1))
-    
-    #Too few observations for full batch to be a problem
-    sess.run(train_op, feed_dict={X: trX, Y: trY, p_keep_input: input_keep, p_keep_hidden: hidden_keep})
+        #Too few observations for full batch to be a problem
+        sess.run(train_op, feed_dict={X: trX, Y: trY, p_keep_input: input_keep, p_keep_hidden: hidden_keep})
+
+    return {
+            'test_error':test_error,
+            'train_error':train_error, 
+            'Precision':prec, 
+            'Recall':rec, 
+            'F1':f1, 
+            'HU':hidden_units, 
+            'IK':input_keep, 
+            'HK':hidden_keep, 
+            'LR':lr, 
+            'RHO':rho,
+            'feature_count':features
+            }
+
+    sess.close()
